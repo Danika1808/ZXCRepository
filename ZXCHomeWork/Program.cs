ee7018be16d19f1349742d5a8267354d2e89adfe
Danil <danikavsilev@gmail.com>
2020-11-25 04:05:17 +0300

    Доделал :angel:

---
+++ ZXCHomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ZXCHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+            var a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = a;
+            }
+            var nAngle = new NAngle(numbers);
+            Console.WriteLine($"Площадь равна = {nAngle.Square(a)}");
+            Console.WriteLine($"Периметр равен = {nAngle.Perimetr(a)}");
+            Console.WriteLine($"Угол {n}-угольника равен = {nAngle.Angle()}");
+        }
+    }
+}
