91743a077be56713495315d7a2ce43737b329d57
Danil <danikavsilev@gmail.com>
2020-11-25 04:34:42 +0300

    Добавил перегрузки

---
+++ ZXCHomeWork/NAngle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZXCHomeWork
+{
+    class NAngle
+    {
+        private int Count { get; }
+        private double Length { get; }
+        public NAngle(int count, double length = double.NaN)
+        {
+            if (double.IsNaN(length))
+            {
+                Console.WriteLine("Введите длину стороны, а то забыли :)");
+                length = double.Parse(Console.ReadLine());
+            }
+            Count = count;
+            Length = length;
+        }
+        public NAngle(double length, int count = 0)
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Введите количество сторон, а то забыли :)");
+                count = int.Parse(Console.ReadLine());
+            }
+            Count = count;
+            Length = length;
+        }
+        private double GetRadius()
+        {
+            return Length / (2 * Math.Sin(Math.PI / Count));
+        }
+
+        public double Square()
+        {
+            var radius = GetRadius();
+            return (Count / 2.0f) * Math.Pow(radius,2) * Math.Sin(2 * Math.PI / Count);
+        }
+
+        public double Perimetr()
+        {
+            return Length * Count;
+        }
+        public double Angle()
+        {
+            return (Count - 2.0f) * 180 / Count;
+        }
+    }
+}
---
+++ ZXCHomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZXCHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = double.Parse(Console.ReadLine());
+            var nAngle = new NAngle(n, a);
+            Console.WriteLine($"Площадь равна = {nAngle.Square()}");
+            Console.WriteLine($"Периметр равен = {nAngle.Perimetr()}");
+            Console.WriteLine($"Угол n-угольника равен = {nAngle.Angle()}");
+        }
+    }
+}
